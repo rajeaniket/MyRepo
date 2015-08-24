6ef7d323df76035ef1c74d3bdb92a6589d011a9c
rajeaniket <aniket.6705@gmail.com>
2015-08-24 14:44:21 +0530

    Created a rectangle and tested area and perimeter

---
+++ src/main/org/barclays/Rectangle.java
@@ -0,0 +1,24 @@
+package org.barclays;
+
+public class Rectangle
+{
+    private int length;
+    private int breadth;
+
+    public Rectangle(int length, int breadth)
+    {
+        this.length = length;
+        this.breadth = breadth;
+    }
+
+    public int area()
+    {
+        return this.length * this.breadth;
+    }
+
+    public int perimeter()
+    {
+        return 2 * (this.length + this.breadth);
+    }
+
+}
